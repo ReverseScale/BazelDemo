9de7df70887c1c25feb6468e8003cbc1975821a7
zhuge <zhuge@bilibili.com>
2018-08-20 11:33:41 +0800

    示例

---
+++ sample/App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BazelDemo
+//
+//  Created by zhuge on 2018/8/20.
+//  Copyright © 2018年 ZG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sample/App/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BazelDemo
+//
+//  Created by zhuge on 2018/8/20.
+//  Copyright © 2018年 ZG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
